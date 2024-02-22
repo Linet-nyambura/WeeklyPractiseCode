555e0726158f083ec2b8e997cecba5f658620f03
Linet-nyambura <linetnyambura27@gmail.com>
2024-02-22 15:26:27 +0300

    my weekely challenge

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What's your name?")
+age = input("What's your age?")
+location = input("What's your location?")
+print('Hello {}, you are {} years old and live in {}'.format(name, age, location))
